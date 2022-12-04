644b461741dd708a0425d34297f480769323eb9d
MarceloJrdaLuz <marcelojuniordaluz@gmail.com>
2022-12-04 19:39:10 -0300

    alteracao aws s3

--- src/config/multer.js
+++ src/config/multer.js
@@ -20,7 +20,11 @@ const storageTypes = {
     }
   }),
   s3: multerS3({
-    s3: new aws.S3(),
+    s3: new aws.S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID_QUADRO,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_QUADRO,
+      region: process.env.AWS_DEFAULT_REGION_QUADRO
+    }),
     bucket: process.env.BUCKET_NAME,
     contentType: multerS3.AUTO_CONTENT_TYPE,
     acl: "public-read",
